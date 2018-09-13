d7bd1924ccbe4f29a1207b6322b835a9ac51f718
Liana Bezhanyan <liana_bezhanyan@edu.aua.am>
2018-09-13 21:00:36 +0200

    first commit

---
+++ homework2.js
@@ -0,0 +1,18 @@
+
+const a = 'password';
+const b ='locker';
+const c = 21;
+const age = 18
+const d = 'Dear';
+const e = 'human';
+
+alert("sf")
+
+if ( a!== 'password' || a===b && b===c )
+{console.log ("you're a robot");}
+else {console.log (d +' '+ e );}
+	
+
+if ( age < c)
+	{console.log ('Welcome'+' ' + age / 9+" "+' '+'the'+' '+ "club!!!!!")}
+else { console.log("nonono")}
